5ca22fc2cdb2f1e12e4083052d9f797c4a1c84f9
Sean Treadway <sean@soundcloud.com>
2012-04-07 20:51:20 +0200

    Rename package, UUID as []byte alias, QuickCheck

--- uuid.go
+++ uuid.go
@@ -1,26 +1,29 @@
-package uuid
+package simpleuuid
 
 import (
+	"bytes"
 	urandom "crypto/rand"
 	"encoding/binary"
 	"encoding/hex"
 	"errors"
 	"math/big"
-	mrandom "math/rand"
+	prandom "math/rand"
+	"strings"
 	"time"
 )
 
 const (
 	gregorianEpoch = 0x01B21DD213814000
+	size           = 16
 	variant        = 0x8000 // sec. 4.1.1
 	version1       = 0x1000 // sec. 4.1.3
 )
 
 var (
-	parseError = errors.New("Could not parse UUID")
-	max13bit   = big.NewInt(2 ^ 13)
-	max16bit   = big.NewInt(2 ^ 16)
-	max32bit   = big.NewInt(2 ^ 32)
+	parseErrorLength = errors.New("Could not parse UUID due to mistmatched length")
+	max13bit         = big.NewInt(1 << 13)
+	max16bit         = big.NewInt(1 << 16)
+	max32bit         = big.NewInt(1 << 32)
 )
 
 /*
@@ -36,27 +39,31 @@ var (
    |                         node (2-5)                            |
    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 */
-type UUID struct {
-	bytes []byte
-}
+type UUID []byte
 
 type uuidTime int64
 
-func NewBytes(bytes []byte) (*UUID, error) {
-	if len(bytes) < 16 {
-		return nil, parseError
+// Makes a copy of the UUID. Assumes the provided UUID is valid
+func Copy(uuid UUID) UUID {
+	dup, _ := NewBytes(uuid)
+	return dup
+}
+
+func NewBytes(bytes []byte) (UUID, error) {
+	if len(bytes) != size {
+		return nil, parseErrorLength
 	}
 
 	// Copy out this slice so not to hold a reference to the container
-	b := make([]byte, 16)
-	copy(b, bytes[0:16])
+	b := make([]byte, size)
+	copy(b, bytes[0:size])
 
-	return &UUID{ b }, nil
+	return UUID(b), nil
 }
 
-func NewTime(t time.Time) (*UUID, error) {
-	bytes := make([]byte, 16)
-	ts := fromUnixNano(t.UnixNano())
+func NewTime(t time.Time) (UUID, error) {
+	bytes := make([]byte, size)
+	ts := fromUnixNano(t.UTC().UnixNano())
 
 	// time
 	binary.BigEndian.PutUint32(bytes[0:4], uint32(ts&0xffffffff))
@@ -70,41 +77,69 @@ func NewTime(t time.Time) (*UUID, error) {
 	binary.BigEndian.PutUint16(bytes[10:12], uint16(rand(max16bit)))
 	binary.BigEndian.PutUint32(bytes[12:16], uint32(rand(max32bit)))
 
-	return &UUID{bytes}, nil
+	return UUID(bytes), nil
 }
 
-func (me *UUID) Time() time.Time {
+func NewString(s string) (UUID, error) {
+	normalized := strings.Replace(s, "-", "", -1)
+
+	if hex.DecodedLen(len(normalized)) != size {
+		return nil, parseErrorLength
+	}
+
+	bytes, err := hex.DecodeString(normalized)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return UUID(bytes), nil
+}
+
+// Returns the time in UTC
+func (me UUID) Time() time.Time {
 	nsec := me.Nanoseconds()
-	return time.Unix(nsec/1e9, nsec%1e9)
+	return time.Unix(nsec/1e9, nsec%1e9).UTC()
 }
 
-func (me *UUID) Nanoseconds() int64 {
-	time_low := uuidTime(binary.BigEndian.Uint32(me.bytes[0:4]))
-	time_mid := uuidTime(binary.BigEndian.Uint16(me.bytes[4:6]))
-	time_hi := uuidTime((binary.BigEndian.Uint16(me.bytes[6:8]) & 0x0fff))
+func (me UUID) Nanoseconds() int64 {
+	time_low := uuidTime(binary.BigEndian.Uint32(me[0:4]))
+	time_mid := uuidTime(binary.BigEndian.Uint16(me[4:6]))
+	time_hi := uuidTime((binary.BigEndian.Uint16(me[6:8]) & 0x0fff))
 
 	return toUnixNano((time_low) + (time_mid << 32) + (time_hi << 48))
 }
 
-func (me *UUID) Version() int8 {
-	return int8((binary.BigEndian.Uint16(me.bytes[6:8]) & 0xf000) >> 12)
+func (me UUID) Version() int8 {
+	return int8((binary.BigEndian.Uint16(me[6:8]) & 0xf000) >> 12)
 }
 
-func (me *UUID) Variant() int8 {
-	return int8((binary.BigEndian.Uint16(me.bytes[8:10]) & 0xe000) >> 13)
+func (me UUID) Variant() int8 {
+	return int8((binary.BigEndian.Uint16(me[8:10]) & 0xe000) >> 13)
 }
 
-func (me *UUID) String() string {
-	return hex.EncodeToString(me.bytes[0:4]) + "-" +
-		hex.EncodeToString(me.bytes[4:6]) + "-" +
-		hex.EncodeToString(me.bytes[6:8]) + "-" +
-		hex.EncodeToString(me.bytes[8:10]) + "-" +
-		hex.EncodeToString(me.bytes[10:16])
+func (me UUID) String() string {
+	return hex.EncodeToString(me[0:4]) + "-" +
+		hex.EncodeToString(me[4:6]) + "-" +
+		hex.EncodeToString(me[6:8]) + "-" +
+		hex.EncodeToString(me[8:10]) + "-" +
+		hex.EncodeToString(me[10:16])
 }
 
-// Treat this as immutable
-func (me *UUID) Bytes() ([]byte) {
-	return me.bytes
+func (me UUID) Compare(other UUID) int {
+	nsMe := me.Nanoseconds()
+	nsOther := other.Nanoseconds()
+	if nsMe > nsOther {
+		return 1
+	} else if nsMe < nsOther {
+		return -1
+	}
+	return bytes.Compare(me[8:], other[8:])
+}
+
+// Treat the slice returned as immutable
+func (me UUID) Bytes() []byte {
+	return me
 }
 
 // Utility functions
@@ -112,7 +147,7 @@ func (me *UUID) Bytes() ([]byte) {
 func rand(max *big.Int) int64 {
 	i, err := urandom.Int(urandom.Reader, max)
 	if err != nil {
-		return mrandom.Int63n(max.Int64())
+		return prandom.Int63n(max.Int64())
 	}
 	return i.Int64()
 }
--- uuid_test.go
+++ uuid_test.go
@@ -1,13 +1,17 @@
-package uuid
+package simpleuuid
 
 import (
+	"bytes"
+	"strings"
 	"testing"
+	"testing/quick"
 	"time"
 )
 
 var (
-	zero = []byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }
-	url = []byte{0x6b, 0xa7, 0xb8, 0x11, 0x9d, 0xad, 0x11, 0xd1, 0x80, 0xb4, 0x00, 0xc0, 0x4f, 0xd4, 0x30, 0xc8 }
+	zero      = []byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}
+	url       = []byte{0x6b, 0xa7, 0xb8, 0x11, 0x9d, 0xad, 0x11, 0xd1, 0x80, 0xb4, 0x00, 0xc0, 0x4f, 0xd4, 0x30, 0xc8}
+	urlString = "6ba7b811-9dad-11d1-80b4-00c04fd430c8"
 )
 
 func TestNewBytes(t *testing.T) {
@@ -26,9 +30,52 @@ func TestNewTimeRoundTrip(t *testing.T) {
 	}
 
 	then := uuid.Time()
-	if now != then {
+	if now.UTC() != then {
 		t.Error("UUID should parse and generate", now, then)
 	}
+}
+
+func TestNewString(t *testing.T) {
+	uuid1, err := NewString(urlString)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if uuid1.String() != urlString {
+		t.Error("Strings do not match", uuid1.String(), urlString)
+	}
+
+	uuid2, err := NewString(strings.Replace(urlString, "-", "", -1))
+	if err != nil {
+		t.Error(err)
+	}
+
+	if uuid2.String() != uuid1.String() {
+		t.Error("Stripping dashes should not affect string parsing", uuid1, uuid2)
+	}
+}
+
+func TestBadNewString(t *testing.T) {
+	_, err := NewString("0000")
+	if err == nil {
+		t.Error("Should fail on short GUID")
+	}
+
+	_, err = NewString("00000000000000000000000000000000000000000")
+	if err == nil {
+		t.Error("Should fail on long GUID")
+	}
+
+	_, err = NewString("0000------------------------0000")
+	if err == nil {
+		t.Error("Should fail on missing digits")
+	}
+
+	_, err = NewString("-0--000-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0--0--")
+	if err != nil {
+		t.Error("Should ignore dashes")
+	}
+
 }
 
 func TestFormatString(t *testing.T) {
@@ -38,7 +85,7 @@ func TestFormatString(t *testing.T) {
 		t.Error(err)
 	}
 
-	if uuid.String() != "6ba7b811-9dad-11d1-80b4-00c04fd430c8" {
+	if uuid.String() != urlString {
 		t.Error("UUID should have correct string", uuid.String())
 	}
 }
@@ -103,3 +150,92 @@ func TestBytes(t *testing.T) {
 	}
 }
 
+func TestCompare(t *testing.T) {
+	u1, err := NewBytes(url)
+	if err != nil {
+		t.Error(err)
+	}
+
+	u2, err := NewBytes(url)
+	if err != nil {
+		t.Error(err)
+	}
+
+	u3, err := NewBytes(zero)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if bytes.Compare(u1, u2) != 0 {
+		t.Error("Should be equal", u1, u2)
+	}
+
+	if bytes.Compare(u1, u3) <= 0 {
+		t.Error("Should be greater", u1, u3)
+	}
+
+	if bytes.Compare(u3, u1) >= 0 {
+		t.Error("Should be less", u1, u3)
+	}
+}
+
+// Conditions
+
+func TestUnixTimeAt100NanoResolution(t *testing.T) {
+	f := func(sec, nsec uint32) bool {
+		now := time.Unix(int64(sec), int64(nsec))
+		u1, _ := NewTime(now)
+
+		return u1.Time().UnixNano()/100 == now.UnixNano()/100
+	}
+
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestInequalityForTime(t *testing.T) {
+	f := func(sec, nsec uint32) bool {
+		time := time.Unix(int64(sec), int64(nsec))
+		u1, _ := NewTime(time)
+		u2, _ := NewTime(time)
+
+		return u1.Compare(u2) != 0
+	}
+
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestPositiveTime(t *testing.T) {
+	f := func(sec, nsec uint32) bool {
+		time := time.Unix(int64(sec), int64(nsec))
+		u1, _ := NewTime(time)
+
+		return u1.Nanoseconds() > 0
+	}
+
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestOrdering(t *testing.T) {
+	f := func(sec1, nsec1, sec2, nsec2 uint32) bool {
+		time1 := time.Unix(int64(sec1), int64(nsec1))
+		time2 := time.Unix(int64(sec2), int64(nsec2))
+
+		u1, _ := NewTime(time1)
+		u2, _ := NewTime(time2)
+
+		if time1.UnixNano() > time2.UnixNano() {
+			return u1.Compare(u2) > 0
+		}
+		return u1.Compare(u2) < 0
+	}
+
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
